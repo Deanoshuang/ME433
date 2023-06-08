dd931b5cd2fa8882cc994c72e87b4bb99b7bc447
Deanoshuang <deanhuang2023@u.northwestern.edu>
2023-06-08 13:41:57 -0500

    update

--- HW6/main.c
+++ HW6/main.c
@@ -1,26 +1,40 @@
 #include "i2c_master_noint.h"
 #include "nu32dip.h"
 
-void turn_GP7(void);
+#define TARGET_ADDRESS 0b0100000<<1
+#define TARGET_REG 0x09
+#define TARGET_WRITE_REG 0x0A
+#define TARGET_WRITE_VAL_ON 0b10000000
+#define TARGET_WRITE_VAL_OFF 0b00000000
+#define GP7_DELAY_COUNT 3000000
+
+void toggle_GP7(void);
 unsigned char GP0_read(unsigned char a, unsigned char reg);
 void i2c_write(unsigned char a, unsigned char reg, unsigned char val);
+
 int main() {
-    unsigned char sending=0b0100000<<1;
     unsigned char reading;
+
     i2c_master_setup();
     NU32DIP_Startup();
+
     while (1) {
-        turn_GP7();
-        reading = GP0_read(sending, 0x09);
-        if (reading) {i2c_write(sending, 0x0A, 0b10000000);}
-        else { i2c_write(sending, 0x0A, 0b00000000);}
+        toggle_GP7();
+        reading = GP0_read(TARGET_ADDRESS, TARGET_REG);
+        if (reading) {
+            i2c_write(TARGET_ADDRESS, TARGET_WRITE_REG, TARGET_WRITE_VAL_ON);
+        } else { 
+            i2c_write(TARGET_ADDRESS, TARGET_WRITE_REG, TARGET_WRITE_VAL_OFF);
+        }
     }
 }
-void turnOn_GP7(void) {
+
+void toggle_GP7(void) {
     LATAbits.LATA4 = !LATAbits.LATA4;
     _CP0_SET_COUNT(0);
-    while (_CP0_GET_COUNT() < 3000000) {}
+    while (_CP0_GET_COUNT() < GP7_DELAY_COUNT) {}
 }
+
 unsigned char GP0_read(unsigned char a, unsigned char reg) {
     i2c_master_start();
     i2c_master_send(a);
@@ -32,6 +46,7 @@ unsigned char GP0_read(unsigned char a, unsigned char reg) {
     i2c_master_stop();
     return r;
 }
+
 void i2c_write(unsigned char a, unsigned char reg, unsigned char val) {
     i2c_master_start();
     i2c_master_send(a);
