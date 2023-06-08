a025f2e6a2c2eaae6cb0603a5a66fbdfe6b873ef
Deanoshuang <deanhuang2023@u.northwestern.edu>
2023-06-08 14:07:19 -0500

    update

--- HW14/hw14.c
+++ HW14/hw14.c
@@ -1,16 +1,30 @@
 #include "servo.h"
 #include "nu32dip.h"
 
-int main(){
+#define ANGLE1 45
+#define ANGLE2 135
+#define DELAY_S 4
+
+void delay(int delay_s);
+void set_angle_and_wait(int angle, int delay_s);
+
+int main() {
     NU32DIP_Startup();
     ServoInit();
-    while(1){
-        SetAng(45);
-        _CP0_SET_COUNT(0);
-        while(_CP0_GET_COUNT() < 24000000*4){}
-        SetAng(135);
-        _CP0_SET_COUNT(0);
-        while(_CP0_GET_COUNT() < 24000000*4){}
-        }
+
+    while(1) {
+        set_angle_and_wait(ANGLE1, DELAY_S);
+        set_angle_and_wait(ANGLE2, DELAY_S);
+    }
     return 0;
+}
+
+void delay(int delay_s) {
+    unsigned int delay_count = _CP0_GET_COUNT() + delay_s * 24000000;
+    while(_CP0_GET_COUNT() < delay_count) {}
+}
+
+void set_angle_and_wait(int angle, int delay_s) {
+    SetAng(angle);
+    delay(delay_s);
 }
